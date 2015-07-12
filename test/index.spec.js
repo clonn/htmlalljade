132e7972a1f0157d6ad0ab8e5b968346addaedaf
clonn <clonncd@gmail.com>
2015-07-12 15:18:16 +0800

    update test

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,13 +1,13 @@
 require("chai").should();
 
-var exec = require("../index");
+var htmlalljade = require("../index");
 var fs = require("fs");
 var fsX = require("fs-extra");
 
 describe('get jade in every folder and compile', function(done) {
   
   it('execute html conver to jade', function(done) {
-    exec.execute("testFolder/src", "testFolder/dest", function (data, stat) {
+    htmlalljade.execute("testFolder/src", "testFolder/dest", function (data, stat) {
     
       var result = fs.readFileSync("testFolder/dest/some/path/wowowow.jade", "utf8")
       result.indexOf("div").should.be.above(0);

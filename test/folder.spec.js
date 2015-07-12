cc9ede0e6fd854555a991b23c37b876cfea640a0
clonn <clonncd@gmail.com>
2015-07-12 15:07:12 +0800

    update test fucntion

--- test/folder.spec.js
+++ test/folder.spec.js
@@ -1,28 +1,12 @@
 require("chai").should();
 
-var htmlalljade = require("../index");
+var htmlalljade = require("..");
 var fs = require("fs-extra");
 
 describe('Path walker', function() {
   it(', read path form execute path', function(done) {
-    var path = htmlalljade.resolvePath(".tmp/user/kerker");
+    var path = htmlalljade.resolvePath(".tmp/user/kerker", "kerker");
     (path.indexOf("/user/kerker")).should.be.above(0);
-    return done();
-  });
-});
-
-describe('Get folder and build a folder path', function() {
-  var pathDir = '.tmp/some/path';
-
-  it(', read folder and get folder', function(done) {
-    fs.mkdirsSync(pathDir);
-    var result = fs.ensureDirSync(pathDir);
-    console.log(result);
-    done();
-  });
-
-  after(function (done) {
-    fs.removeSync('.tmp/some/')
     return done();
   });
 });
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,19 +1,24 @@
+require("chai").should();
 
 var exec = require("../index");
-
+var fs = require("fs");
+var fsX = require("fs-extra");
 
 describe('get jade in every folder and compile', function(done) {
-  it(', have to compile all jade and walk recursive', function (done) {
-    return done();
-  });
-
-  it.only('run', function(done) {
+  
+  it('execute html conver to jade', function(done) {
     exec.execute("testFolder/src", "testFolder/dest", function (data, stat) {
-      console.log("test ---")
-      console.log(data);
-      console.log(stat);
-      return done();  
+    
+      var result = fs.readFileSync("testFolder/dest/some/path/wowowow.jade", "utf8")
+      result.indexOf("div").should.be.above(0);
+
+      return done();
     });
     
+  });
+
+  after(function (done) {
+    fsX.removeSync('testFolder/dest/some')
+    return done();
   });
 });
